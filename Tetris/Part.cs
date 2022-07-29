aaf6048d863a35c708818b11bfa324c659830342
Artem Mendruk <artemmendruk@gmail.com>
2022-07-29 17:11:33 +0200

    Исправлен стиль кода

--- Tetris/Part.cs
+++ Tetris/Part.cs
@@ -1,24 +1,16 @@
-﻿namespace Tetris
+﻿namespace Tetris;
+
+public class Part
 {
-    public class Part
+    public int X;
+    public int Y;
+
+    public Part(int X, int Y, Brush brush)
     {
-        private Brush brush;
-        public int X;
-        public int Y;
-
-        public Part(int X,int Y,Brush brush)
-        {
-            this.X = X;
-            this.Y = Y;
-            this.brush = brush;
-        }
-
-        public Brush Brush
-        {
-            get
-            {
-                return this.brush;
-            }
-        }
+        this.X = X;
+        this.Y = Y;
+        Brush = brush;
     }
+
+    public Brush Brush { get; }
 }
